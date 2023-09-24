d0d2a9a5f18d4f4e9b16a215e4004fed508117df
bobby02k <133523857+bobby02k@users.noreply.github.com>
2023-09-24 11:05:00 +0200

    Add files via upload

--- binary_conversion/binary_analysis.cpp
+++ binary_conversion/binary_analysis.cpp
@@ -7,22 +7,22 @@ function 'binary' that reads
 binary digits of an integer number n
 using bitwise operator AND '&'
 */
-void binary(int n){
-    unsigned int mask = 0;
-    for(int i = 31; i >= 0; i--){
+void binary(int n, int bits = 32){
+    unsigned int mask = pow(2,bits-1);
+    for(int i = bits-1; i >= 0; i--){
         if((i+1) % 4 == 0)cout << " ";
-        mask = pow(2,i); //shift of the i-th bit left->right
         bool bit = mask & n;
         cout << bit;
+        mask = mask >> 1;//shift of the i-th bit left->right
     }
     cout << endl;
 }
 
 int main(){
 
-    int n = 5;
+    int n = 64;
     binary(n);
-    binary(-n);
+    binary((n>>1));
 
     return 0;
 }
